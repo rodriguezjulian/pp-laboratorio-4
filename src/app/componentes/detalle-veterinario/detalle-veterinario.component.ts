1ee35cc5835814c9fb50e06c3d4201a8b2888cb8
rodriguezjulian <julian_rodriguez99@hotmail.com>
2024-11-18 17:04:49 -0300

    mensaje de aun no hay informacion para mostrar en el listado de veterinarios

--- src/app/componentes/detalle-veterinario/detalle-veterinario.component.ts
+++ src/app/componentes/detalle-veterinario/detalle-veterinario.component.ts
@@ -1,8 +1,9 @@
 import { Component, Input } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-detalle-veterinario',
   standalone: true,
+  imports : [CommonModule],
   templateUrl: './detalle-veterinario.component.html',
   styleUrls: ['./detalle-veterinario.component.css']
 })
